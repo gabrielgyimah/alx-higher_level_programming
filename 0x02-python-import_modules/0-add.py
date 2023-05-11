1e54dfc4170ec0efd9b413b5a97ce278bed5eedf
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-05-11 15:22:48 -0700

    Task 0 & README

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from add_0 import add
+
+def main():
+    a = 1
+    b = 2
+    ans = add(a, b)
+
+    print("{:d} + {:d} = {:d}".format(a, b, ans))
+
+if __name__ == "__main__":
+    main()
