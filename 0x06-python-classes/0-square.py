45a3d2ae5e005c6bcabd51ada4deaaa734c86eff
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-05-30 16:46:35 +0000

    an empty class Square that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+"""Defines a template or creating and interfacing square objects"""
+
+class Square:
+    """Defines the template"""
+    pass#!/usr/bin/python
+
+"""Defines a template or creating and interfacing square objects"""
+
+class Square:
+    """Defines the template"""
+    pass
