b57abe18cf424233e5f075a778c9c33422a705c5
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-05-30 17:08:52 +0000

    a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+
+"""Defines a new template of type square"""
+
+
+class Square:
+    """The deinition of class Square"""
+
+
+    def __init__(self, size) -> None:
+        self.__size = size
