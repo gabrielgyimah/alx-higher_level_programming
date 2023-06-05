59316ce53a2dfac9ba641f27618683cf08ba4796
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-06-05 12:57:15 -0700

    Task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-"""Defines an empty Rectangle class"""
+"""Defines an empty Rectangle"""
 
 
 class Rectangle:
-    """Defines an empty Rectangle class"""
+    """Defines an empty Rectangle"""
 
     pass
