154913637217a2f44f569b5a0498956035b7eb91
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-06-06 04:44:38 -0700

    Tasks

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(i=[0]):
+    i[0] += 1
+    return "%s, " % ("BestSchool" * i[0])
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+"""Creates instances of the class LockedClass"""
+
+
+class LockedClass(ObjName):
+    """Creates instances of the class LockedClass"""
+
+    __slots__ = ['first_name']
