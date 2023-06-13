4a6f4003871ea659f832bf6889c2c30ad2056afe
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-06-13 10:31:51 -0700

    a script that adds all arguments to a Python list, and then save them to a file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,38 @@
+#!/usr/bin/ptyhon3
+
+"""Adds all command line args to a list"""
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_item():
+    """
+    1. Adds all command line args to a list
+    2. Save them to a file
+    3. The list must be saved as a JSON representation
+    4. file must be named add_item.json
+    5. If the file doesn’t exist, it should be created
+    6. Don’t need to manage file permissions / exceptions.
+    """
+
+    my_list = []
+
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except Exception:
+        my_list = save_to_json_file(my_list, "add_item.json")
+
+    if len(sys.argv) < 2:
+        return
+
+    for item in sys.argv:
+        if item is sys.argv[0]:
+            continue
+        my_list.append(item)
+
+    save_to_json_file(my_list, "add_item.json")
+
+
+add_item()
