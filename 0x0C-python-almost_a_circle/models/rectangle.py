421f2a81c20c45f61fd08e7d338a73fb7d146c9e
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-06-19 14:02:34 -0700

    Rectangle module

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,35 +1,19 @@
 #!/usr/bin/python3
 
-"""Rectangle Module."""
+"""Rectangle Module"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Represents reactangle."""
+    """Creates rectangle objects/ instances"""
 
     def __init__(self, width, height, x=0, y=0, id=None) -> None:
-        """Initialize a reactangle object."""
+        """Initializes instance of the rectangle class"""
         super().__init__(id)
-        if not type(width) is int:
-            raise TypeError("width must be an integer")
-        if not type(height) is int:
-            raise TypeError("height must be an integer")
-        if not type(x) is int:
-            raise TypeError("x must be an integer")
-        if not type(y) is int:
-            raise TypeError("y must be an integer")
-        if width <= 0:
-            raise ValueError("width must be > 0")
-        if height <= 0:
-            raise ValueError("height must be > 0")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-        if y < 0:
-            raise ValueError("y must be >= 0")
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
