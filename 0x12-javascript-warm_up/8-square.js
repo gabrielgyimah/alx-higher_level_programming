cf13ab0b1712a9bc33a907e403ef17524fc5a951
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-08-15 06:37:18 -0700

    Fixed task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,12 +3,12 @@
 const sizeOfSquare = parseInt(process.argv[2]);
 
 if (!sizeOfSquare) {
-  console.log('Missing size');
+	console.log('Missing size');
 } else {
-  let counter = 0;
+	let counter = 0;
 
-  while (counter < sizeOfSquare) {
-    console.log('x'.repeat(sizeOfSquare));
-    counter++;
-  }
+	while (counter < sizeOfSquare) {
+		console.log('X'.repeat(sizeOfSquare));
+		counter++;
+	}
 }
