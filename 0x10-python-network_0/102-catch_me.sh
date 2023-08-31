51aaf8c4dd1c8ca93d78b074954c5c18128edf92
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-08-31 13:50:48 -0700

    A bBash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request with apice of information
-curl -H "Content-Type: plain/text" -d "You got me!" "$1"
+curl -H "Content-Type: plain/text" -d "You got me!" 0.0.0.0:5000/catch_me
