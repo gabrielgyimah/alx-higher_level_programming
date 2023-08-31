27b9278b72924ceba4481f6d548f1728033615e0
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-08-31 08:28:27 -0700

    displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Displays the size of the body of an HTTP response
 
-if [ "$#" -lt 1 ]; then
-	return
+if [ "$#" != 1 ]; then
+	exit
 else
 	curl -I -s "$1" | awk '/Content-Length/ {print $2}'
 fi
