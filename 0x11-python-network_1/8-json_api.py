1eec8e2ecd59ecf6dffc70848e02a257a9fda886
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-09-01 12:55:49 -0700

    A python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Sends a POST request to http://0.0.0.0:5000/search_user"""
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    """Sends a POST request to http://0.0.0.0:5000/search_user"""
+
+    data = {}
+
+    if sys.argv[1]:
+        data = {'q', sys.argv[1])
+    res = requests.post('http://0.0.0.0:5000/search_user', data=data)
+
+    try:
+        obj = res.json()
+        if not obj:
+            print('No result')
+        else:
+            print(f"[{obj.get('id')}] {obj.get('name')}")
+    except Exception:
+        print("Not a valid JSON")
