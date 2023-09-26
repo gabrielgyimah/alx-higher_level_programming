3a0aa42b13cf99d359ccc37e36277b25b304ed8a
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-09-26 08:32:34 -0700

    Added a script that prints the status code of a request to the console

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-// Display the status code of a GET request.
 
 const request = require('request');
 
-request.get(process.argv[2], on.('res', (res) => {
+request.get(process.argv[2].on('res', (res) => {
 	console.log("code:", res.statusCode);
 });
